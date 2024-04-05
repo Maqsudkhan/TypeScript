f1d29f04b10d27924462be2a6aa016638bc6e895
Maqsudkhan <maqsudtorayev2003@gmail.com>
2024-04-05 11:57:46 +0500

    add file

---
+++ ExerciseOne.js
@@ -0,0 +1,45 @@
+// 1-misol
+// let person_name: string =  "Maqsud";
+// let person_age: number = 22;
+// console.log("Ismi: ", person_name);
+// console.log("Yoshi", person_age);
+// // 2-misol
+// let itemOne: number = 1;
+// if(true){
+//     let itemOne: number = 2;
+// }
+// console.log(itemOne); // outputda 1 natija chiqadi
+// const itemTwo:string = "Hello Maqsud";
+// //itemTwo = "Salom Maqsud"; //Error chiqadi chunki constantani o'zgartirib bo'maydi
+// function FunctionOne(){
+//     var itemThree: boolean = true;
+//     if (itemThree){
+//         var itemThree = false;
+//     }
+//     console.log(itemThree);
+// }
+// FunctionOne();
+
+// 3-misol
+var itemNumberOne = 11;
+var itemBoolean = false;
+var itemString = "Hello Maqsud";
+var itemUndefined = undefined;
+var itemNumberTwo = 1;
+
+var sum = itemNumberOne + itemNumberTwo;
+console.log("Yig'indisi: ", sum);
+
+var addString = itemString + "Qalesiz?";
+console.log("2 ta stringni yig'indisi: ", addString);
+
+var IsTrue = itemBoolean && true;
+console.log("Boolean bilan ishlash: ", IsTrue);
+if (itemUndefined === undefined) {
+    console.log("itemUndefined is undefined");
+}
+else {
+    console.log("itemUndefined is defined");
+}
+
+
---
+++ ExerciseOne.ts
@@ -0,0 +1,52 @@
+// 1-misol
+// let person_name: string =  "Maqsud";
+// let person_age: number = 22;
+// console.log("Ismi: ", person_name);
+// console.log("Yoshi", person_age);
+
+// // 2-misol
+// let itemOne: number = 1;
+// if(true){
+//     let itemOne: number = 2;
+// }
+// console.log(itemOne); // outputda 1 natija chiqadi
+
+// const itemTwo:string = "Hello Maqsud";
+// //itemTwo = "Salom Maqsud"; //Error chiqadi chunki constantani o'zgartirib bo'maydi
+
+// function FunctionOne(){
+//     var itemThree: boolean = true;
+//     if (itemThree){
+//         var itemThree = false;
+//     }
+//     console.log(itemThree);
+// }
+// FunctionOne();
+
+
+// // 3-misol
+// let itemNumberOne:number = 11;
+// let itemBoolean: boolean = false;
+// let itemString: string = "Hello Maqsud"
+// let itemUndefined: undefined = undefined;
+
+// const itemNumberTwo: number =  1;
+// const sum: number = itemNumberOne + itemNumberTwo;
+// console.log("Yig'indisi: ", sum);
+
+// const addString:string = itemString +  " Qalesiz?";
+// console.log("2 ta stringni yig'indisi: " ,addString)
+
+// const IsTrue: boolean = itemBoolean && true
+// console.log("Boolean bilan ishlash: ", IsTrue)
+
+// if(itemUndefined === undefined){
+//     console.log("itemUndefined is undefined");
+// }
+// else{
+//     console.log("itemUndefined is defined");
+// }
+
+
+// 4-misol
+
